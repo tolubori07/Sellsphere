bccf14d8ab6707b616effe7161e573b68cfc915f
Jaabir Adeniji-Bello <tolubori07@gmail.com>
2025-03-16 13:43:49 +0000

    moved

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "dist", "index.html"),
+      path.resolve(__dirname, "./", "frontend", "dist", "index.html"),
     ),
   );
 } else {
